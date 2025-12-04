5a0d21801b086b1aa6a8e97429e7eaa1321351a8
iimtec <techasit.su@ku.th>
2025-12-04 16:00:43 +0700

    This is the final exam problem

---
+++ README.md
@@ -0,0 +1,37 @@
+# Ordering Application (final_project)
+
+This project is using the OOP for the usage of online ordering. Which will show the driver and the customer name, order summary, and order status. 
+
+---
+
+## Features
+- Customer and driver introduction -> showing their names.
+- Order summary -> show the order information including item, customer's name,  status and driver's name.
+- Delivery status -> show that the driver is now delivering the ordered items and the vehicle that they use.
+- Final status -> show that the delivery is successful.
+
+---
+
+## How to run
+Open the terminal in the project folder and run:
+```
+python creature_simulation.py
+```
+
+---
+
+## Project structure
+```
+final-exam/
+├── README.md                           # Project documentation
+|
+├── problem.py                          # main program
+|
+├── screenshot.png                       # screenshot of the final input
+```
+
+---
+
+## Project progress
+
+This project is `100%` finish.
---
+++ problem.py
@@ -0,0 +1,57 @@
+class DeliveryOrder:
+    def __init__(self, customer, item, status = "preparing"):
+        self.customer = customer
+        self.item = item
+        self.status = status
+
+    def assign_driver(driver):
+        return driver
+    
+    def summary(self):
+        return "Order Summary:\n"\
+                f"Item: {self.item}\n"\
+                f"Customer: {self.customer}\n"\
+                f"Status: {self.status}\n"\
+                f"Driver: {d.name}\n"
+
+class Person:
+    def __init__(self,name):
+        self.name = name
+
+    def introduce(self):
+        print(f"Hi, I'm {self.name}.")
+
+class Customer(Person):
+    def __init__(self, name, address):
+        super().__init__(name)
+        self.address = address
+
+    def place_order(item):
+        return DeliveryOrder(item)
+    
+class Driver(Person):
+    def __init__(self, name, vehicle):
+        self.vehicle = vehicle
+        super().__init__(name)
+
+    def deliver(self,order):
+        print(f"{self.name} is delivering {order.item} to {order.customer} using {self.vehicle}")
+
+a = Person("Alice")
+b = Person("Bob")
+d = Driver("David","motorcycle")
+a.introduce()
+b.introduce()
+d.introduce()
+print()
+order_a = DeliveryOrder("Alice","Laptop")
+order_b = DeliveryOrder("Bob","Headphones")
+print(order_a.summary())
+print(order_b.summary())
+print()
+d.deliver(order_a)
+d.deliver(order_b)
+print()
+print("Final Status:")
+print(f"Order for {order_a.item} -> delivered")
+print(f"Order for {order_b.item} -> delivered")
